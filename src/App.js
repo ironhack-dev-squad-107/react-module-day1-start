40d11dccefe41c82e6b3757bbc5da00c6b703a58
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2019-02-18 12:32:36 +0100

    Add dynamic JSX examples

--- src/App.js
+++ src/App.js
@@ -2,9 +2,35 @@ import React, { Component } from "react";
 
 import "./App.css";
 
+const user = {
+  firstName: "Pablo",
+  lastName: "Perez",
+  address: {
+    street1: "24 Rue Blah",
+    zipcode: 75001
+  },
+  emails: ["blah@blah.com", "pablo@perez.com"],
+  avatarUrl: "https://media2.giphy.com/media/HX58Bmu7W1XbO/200.webp"
+};
+
+function displayAvatar() {
+  if (user.avatarUrl) {
+    // no quotes when using {} with HTML attributes
+    return <img src={user.avatarUrl} />;
+  } else {
+    return (
+      <img src="https://s3.amazonaws.com/owler-image/logo/ironhack_owler_20180828_221413_original.png" />
+    );
+  }
+}
+
 // Our first React COMPONENT
 // (a class that extends from React's Component class and defines some content)
 class App extends Component {
+  handleClick() {
+    alert("It's Weird AF");
+  }
+
   // every component class needs a render() method
   // (defines the component's content with the return)
   render() {
@@ -13,8 +39,15 @@ class App extends Component {
     return (
       // use "className" instead of "class" for styling your HTML
       <div className="App container">
-        <h2>JSX is Weird</h2>
+        <h2 onClick={() => this.handleClick()}>JSX is Weird</h2>
+
         <p>This is App component.</p>
+        <p>
+          Welcome, {user.firstName} {user.lastName}! You live at{" "}
+          {user.address.street1}. This is your email: {user.emails[0]}.
+        </p>
+        {/* call a function in {} to display its result */}
+        {displayAvatar()}
       </div>
     );
   }
